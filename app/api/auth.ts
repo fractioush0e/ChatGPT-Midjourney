e7fc59471c58159f7652cb4fdd876d5207199b5a
Syahdan Hayyan Alawy <fractioush0e@gmail.com>
2024-10-14 17:07:36 +0700

    Redeploy 6

--- app/api/auth.ts
+++ app/api/auth.ts
@@ -42,7 +42,7 @@ export function auth(req: NextRequest, modelProvider: ModelProvider) {
   if (serverConfig.needCode && !serverConfig.codes.has(hashedCode) && !apiKey) {
     return {
       error: true,
-      msg: !accessCode ? "empty access code" : "wrong access code",
+      msg: !accessCode ? "akses kode tidak ada" : "akses kode salah",
     };
   }
 
