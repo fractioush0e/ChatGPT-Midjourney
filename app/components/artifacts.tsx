bc994759aa89141ef9668a1708f30f0a7a1810db
Syahdan Hayyan Alawy <fractioush0e@gmail.com>
2024-10-15 10:24:57 +0700

    Redeploy 8
    edit

--- app/components/artifacts.tsx
+++ app/components/artifacts.tsx
@@ -208,7 +208,7 @@ export function Artifacts() {
         <a href={FB_URL} target="_blank" rel="noopener noreferrer">
           <IconButton bordered icon={<GithubIcon />} shadow />
         </a>
-        <div className={styles["artifacts-title"]}>NextChat Artifacts</div>
+        <div className={styles["artifacts-title"]}>ChadGPT Artifacts</div>
         <ArtifactsShareButton
           id={id}
           getCode={() => code}
